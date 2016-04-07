094f30ea508cce49900357478c6bedce677636d6
Jemy <jemygraw@gmail.com>
2016-04-07 20:32:39 +0800

    update sql scripts

--- src/database.sql
+++ src/database.sql
@@ -1,23 +1,51 @@
-CREATE TABLE `user` (
-  `Id` int(11) NOT NULL AUTO_INCREMENT,
-  `Mobile` varchar(20) DEFAULT NULL,
-  `Name` varchar(20) DEFAULT NULL,
-  `Pwd` varchar(32) DEFAULT NULL,
-  `Email` varchar(200) DEFAULT NULL,
-  PRIMARY KEY (`Id`),
-  UNIQUE KEY `Mobile_Unique` (`Mobile`),
-  UNIQUE KEY `Name_Unique` (`Name`),
-  UNIQUE KEY `Email_Unique` (`Email`)
-) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=utf8;
+-- MySQL dump 10.13  Distrib 5.6.15, for osx10.7 (x86_64)
+--
+-- Host: localhost    Database: qlive
+-- ------------------------------------------------------
+-- Server version	5.6.15
 
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
 
-CREATE TABLE `session` (
+--
+-- Current Database: `qlive`
+--
+
+CREATE DATABASE /*!32312 IF NOT EXISTS*/ `qlive` /*!40100 DEFAULT CHARACTER SET utf8 */;
+
+USE `qlive`;
+
+--
+-- Table structure for table `live_stream`
+--
+
+DROP TABLE IF EXISTS `live_stream`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `live_stream` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
-  `user_id` int(10) unsigned DEFAULT NULL,
-  `session_id` varchar(32) DEFAULT NULL,
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=utf8;
+  `stream_id` varchar(100) NOT NULL,
+  `user_id` int(11) DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `Sid_Unique` (`stream_id`)
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
 
+--
+-- Table structure for table `live_video`
+--
+
+DROP TABLE IF EXISTS `live_video`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `live_video` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `title` varchar(100) DEFAULT NULL,
@@ -30,13 +58,51 @@ CREATE TABLE `live_video` (
   `start_time` int(11) DEFAULT NULL,
   `end_time` int(11) DEFAULT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
 
-CREATE TABLE `live_stream` (
+--
+-- Table structure for table `session`
+--
+
+DROP TABLE IF EXISTS `session`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `session` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
-  `stream_id` varchar(100) NOT NULL,
-  `user_id` int(11) DEFAULT NULL,
-  PRIMARY KEY (`id`),
-  UNIQUE KEY `Sid_Unique` (`stream_id`)
-) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=utf8;
+  `user_id` int(10) unsigned DEFAULT NULL,
+  `session_id` varchar(32) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
 
+--
+-- Table structure for table `user`
+--
+
+DROP TABLE IF EXISTS `user`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `user` (
+  `Id` int(11) NOT NULL AUTO_INCREMENT,
+  `Mobile` varchar(20) DEFAULT NULL,
+  `Name` varchar(20) DEFAULT NULL,
+  `Pwd` varchar(32) DEFAULT NULL,
+  `Email` varchar(200) DEFAULT NULL,
+  PRIMARY KEY (`Id`),
+  UNIQUE KEY `Mobile_Unique` (`Mobile`),
+  UNIQUE KEY `Name_Unique` (`Name`),
+  UNIQUE KEY `Email_Unique` (`Email`)
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2016-04-07 20:24:51
